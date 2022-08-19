e74f47a4203a3a05946f415608618602ec11f2f9
kikwaghmare <kikwaghmare@gmail.com>
2022-08-19 11:59:18 +0530

    Committed

---
+++ js/Assignment_4/findTheProduct.js
@@ -0,0 +1,11 @@
+
+const Find_Prod = (array, N) => 
+{
+ 
+     let result = 1;
+        for (let i = 0; i < N; i++)
+            result = result * array[i];
+        return result; 
+
+};
+ 
---
+++ js/Assignment_4/findTheSum.js
@@ -0,0 +1,12 @@
+
+const Find_Sum = (array, N) => 
+{
+  // Getting sum of numbers
+        var sum = array.reduce(function(a, b){
+            return a + b;
+        }, 0);
+        
+        return sum;
+
+};
+ 
